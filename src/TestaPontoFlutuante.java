83eced283886667a80402c7b5e2979e0968acadb
Antonio Gutierrez da Conceicao <antonio@javadeveloper.com.br>
2022-08-31 08:59:45 -0300

    Tipos e variaveis

---
+++ src/TestaPontoFlutuante.java
@@ -0,0 +1,8 @@
+
+public class TestaPontoFlutuante {
+	public static void main(String[] args) {
+		double salario = 1250.70;
+		double integerSalario = 5.0/2;
+		System.out.println("Meu salário é: " + integerSalario);
+	}
+}
